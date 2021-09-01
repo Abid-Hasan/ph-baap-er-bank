87369eda24b9e9e651fb974b4b6797ef7e5dee3e
Abid-Hasan <ahsnigdho@gmail.com>
2021-09-01 11:45:07 +0600

    done

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
-document.getElementById('submit-button').addEventListener('click', function () {
-    performLogin();
-});
+// Login page
+
+const submitButton = document.getElementById('submit-button');
+
+if (submitButton != null) {
+    submitButton.addEventListener('click', function () {
+        performLogin();
+    });
+}
 
 function performLogin() {
     const emailField = document.getElementById('email');
@@ -12,4 +18,46 @@ function performLogin() {
     else {
         window.location.href = 'error.html';
     }
+}
+
+// Account page
+
+const depositButton = document.getElementById('deposit-button');
+if (depositButton != null) {
+    depositButton.addEventListener('click', function () {
+        const depositDisplay = document.getElementById('deposit-display');
+        const previousDeposit = parseFloat(depositDisplay.innerText);
+
+        const balanceDisplay = document.getElementById('balance-display');
+        const previousBalance = parseFloat(balanceDisplay.innerText);
+
+        const depositInput = document.getElementById('deposit-input');
+        const currentDeposit = parseFloat(depositInput.value);
+
+        if (!isNaN(currentDeposit)) {
+            depositDisplay.innerText = previousDeposit + currentDeposit;
+            balanceDisplay.innerText = previousBalance + currentDeposit;
+            depositInput.value = '';
+        }
+    });
+}
+
+const withdrawButton = document.getElementById('withdraw-button');
+if (withdrawButton != null) {
+    withdrawButton.addEventListener('click', function () {
+        const withdrawDisplay = document.getElementById('withdraw-display');
+        const previousWithdraw = parseFloat(withdrawDisplay.innerText);
+
+        const balanceDisplay = document.getElementById('balance-display');
+        const previousBalance = parseFloat(balanceDisplay.innerText);
+
+        const withdrawInput = document.getElementById('withdraw-input');
+        const currentWithdraw = parseFloat(withdrawInput.value);
+
+        if ((!isNaN(currentWithdraw)) && (previousBalance > currentWithdraw)) {
+            withdrawDisplay.innerText = previousWithdraw + currentWithdraw;
+            balanceDisplay.innerText = previousBalance - currentWithdraw;
+            withdrawInput.value = '';
+        }
+    });
 }
