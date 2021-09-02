bf791825152dca76827f79d0b4ceb162c63adb7c
Abid-Hasan <ahsnigdho@gmail.com>
2021-09-03 02:22:45 +0600

    refactoring done

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,74 @@
 // Login page
 
 const submitButton = document.getElementById('submit-button');
-
 if (submitButton != null) {
     submitButton.addEventListener('click', function () {
-        performLogin();
+        const emailField = document.getElementById('email');
+        const passwordField = document.getElementById('password');
+
+        if (emailField.value.length > 0 && passwordField.value.length > 0) {
+            window.location.href = 'account.html';
+        }
+        else {
+            window.location.href = 'error.html';
+        }
     });
-}
-
-function performLogin() {
-    const emailField = document.getElementById('email');
-    const passwordField = document.getElementById('password');
-
-    if (emailField.value.length > 0 && passwordField.value.length > 0) {
-        window.location.href = 'account.html';
-    }
-    else {
-        window.location.href = 'error.html';
-    }
 }
 
 // Account page
 
+function getDisplay(displayType) {
+    return document.getElementById(displayType + '-display');
+}
+
+function getDisplayNumber(display) {
+    return parseFloat(display.innerText);
+}
+
+function getInput(inputType) {
+    return document.getElementById(inputType + '-input');
+}
+
+function getInputNumber(input) {
+    return parseFloat(input.value);
+}
+
+function updateDisplay(display, amount) {
+    display.innerText = amount;
+}
+
+function clearInput(input) {
+    input.value = '';
+}
+
+function performBankingTask(task) {
+    const display = getDisplay(task);
+    const displayNumber = getDisplayNumber(display);
+    const balance = getDisplay('balance');
+    const balanceNumber = getDisplayNumber(balance);
+    const input = getInput(task);
+    const inputNumber = getInputNumber(input);
+
+    if (inputNumber > 0) {
+        if (task == 'deposit') {
+            updateDisplay(display, displayNumber + inputNumber);
+            updateDisplay(balance, balanceNumber + inputNumber);
+        }
+        else if (task == 'withdraw' && inputNumber <= balanceNumber) {
+            updateDisplay(display, displayNumber + inputNumber);
+            updateDisplay(balance, balanceNumber - inputNumber);
+        }
+    }
+
+    clearInput(input);
+}
+
 const depositButton = document.getElementById('deposit-button');
 if (depositButton != null) {
-    depositButton.addEventListener('click', function () {
-        const depositDisplay = document.getElementById('deposit-display');
-        const previousDeposit = parseFloat(depositDisplay.innerText);
-
-        const balanceDisplay = document.getElementById('balance-display');
-        const previousBalance = parseFloat(balanceDisplay.innerText);
-
-        const depositInput = document.getElementById('deposit-input');
-        const currentDeposit = parseFloat(depositInput.value);
-
-        if (!isNaN(currentDeposit)) {
-            depositDisplay.innerText = previousDeposit + currentDeposit;
-            balanceDisplay.innerText = previousBalance + currentDeposit;
-            depositInput.value = '';
-        }
-    });
+    depositButton.addEventListener('click', performBankingTask('deposit'));
 }
 
 const withdrawButton = document.getElementById('withdraw-button');
 if (withdrawButton != null) {
-    withdrawButton.addEventListener('click', function () {
-        const withdrawDisplay = document.getElementById('withdraw-display');
-        const previousWithdraw = parseFloat(withdrawDisplay.innerText);
-
-        const balanceDisplay = document.getElementById('balance-display');
-        const previousBalance = parseFloat(balanceDisplay.innerText);
-
-        const withdrawInput = document.getElementById('withdraw-input');
-        const currentWithdraw = parseFloat(withdrawInput.value);
-
-        if ((!isNaN(currentWithdraw)) && (previousBalance >= currentWithdraw)) {
-            withdrawDisplay.innerText = previousWithdraw + currentWithdraw;
-            balanceDisplay.innerText = previousBalance - currentWithdraw;
-            withdrawInput.value = '';
-        }
-    });
+    withdrawButton.addEventListener('click', performBankingTask('withdraw'));
 }
